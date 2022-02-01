95e97bafd24dabf0d6c42aa7dac9742526278e51
Paul Muller <pmuller@clearchoice.com>
2022-02-01 14:54:11 -0700

    improved SQL server findColumns

--- SQLserver/findColumns.sql
+++ SQLserver/findColumns.sql
@@ -1,10 +1,12 @@
-select s.name as schemaName, 
-       o.name as tableName, 
-	   c.name as columnName, c.column_id
-  from sys.schemas s 
-       join sys.objects o on (s.schema_id = o.schema_id)
-	   join sys.columns c on (o.object_id = c.object_id)
+select s.name as SchemaName, 
+       o.name as ObjectName, o.type_desc as ObjectType, o.is_ms_shipped,
+       c.name as ColumnName, c.column_id, c.is_nullable
+  from sys.objects o
+       inner join sys.schemas s on o.schema_id = s.schema_id
+       inner join sys.columns c on o.object_id = c.object_id
  where 1=1
-   and s.name = 'dbo'
-   and c.name like '%REMOVAL_TI%'
- order by 1,2,4;
+   --and s.name = 'dbo'
+   --and t.name = 'VW_Sales_Activity'
+   and c.name like '%pipeline%callback%'
+ order by s.name, o.name, c.column_id
+;
